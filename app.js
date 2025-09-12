e8141fd2369a04c1bfc486a220fdb6ec158b9023
Lonsky23 <mitramarlon2003@gmail.com>
2025-09-12 22:51:33 +0800

    Add the inventory item crud

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 import { app } from './config.js';
 import authRoute from './auth-module/auth.js';
 import userRoute from './user-module/user.js';
+import inventoryItem from './inventory-module/InventoryItem.js'
 import supplierRoute from './procurement-module/supplier.js';
 import supplyItemRoute from './procurement-module/supply-item.js';
 import purchaseOrderRoute from './procurement-module/purchase-order.js';
 import purchaseRequestRoute from './procurement-module/purchase-request.js';
 import purchaseOrderItemRoute from './procurement-module/purchase-order-item.js';
 
+
 app.use('/api/auth', authRoute);
+app.use('/api/inventory-item',inventoryItem)
 app.use('/api/purchase-orders', purchaseOrderRoute);
 app.use('/api/purchase-order-items', purchaseOrderItemRoute);
 app.use('/api/purchase-requests', purchaseRequestRoute);
 app.use('/api/suppliers', supplierRoute);
 app.use('/api/supply-items', supplyItemRoute)
 app.use('/api/users', userRoute);
+
 
 const PORT = process.env.PORT || 6000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ inventory-module/InventoryItem.js
@@ -0,0 +1,134 @@
+import express, { response } from "express";
+import { supabase } from "../config.js";
+
+const router = express.Router();
+const table = 'inventory_item';
+
+
+
+router.post("/create", async (req, res) => {
+    const body = req.body;
+    const {data, error} = await supabase
+    .from(table)
+    .insert(body)
+    .select('*')
+    .single();
+
+    if (error) return res.status(500).json({message: error.message})
+
+    return res.status(201).json(data)
+})
+
+router.post("/update", async (req, res) => {
+    const { skuid } = req.query
+    const updateRequest = req.body;
+
+    const {data : existing, error : existingErr } = await supabase
+    .from(table)
+    .select('*')
+    .eq('skuid', skuid)
+    .eq('is_deleted', false)
+    .maybeSingle()
+
+    if(existingErr) { return res.status(404).json({message: existingErr.message})}
+
+    if(!existing) { return res.status(404).json({message: "Skuid not found."})}
+ 
+    const { data, error } = await supabase
+        .from(table)
+        .update(updateRequest)
+        .eq('skuid', skuid)
+        .select('*')
+        .single();
+
+    if (error) return res.status(500).json({ message: error.message });
+
+    return res.status(200).json(data);
+})
+
+router.get("/find-by-skuid", async (req, res) => {
+    const {skuid}  = req.query
+
+    const {data, error} = await supabase 
+    .from(table)
+    .select('name, category, cost, unit_measurement')
+    .eq('skuid', skuid)
+    .eq('is_deleted', false)
+    .maybeSingle()
+
+    if(!data) {
+        return res.status(404).json("No inventory item with skuid " + skuid + " found.")
+    }
+    if (error) return res.status(500).json({ message: error.message });
+
+    return res.status(200).json(data)
+})
+
+
+router.get("/find-by-category", async (req, res) => {
+    const {category} = req.query;
+
+
+    const {data: existing, error : existingErr} = await supabase
+    .from(table)
+    .select('*')
+    .eq('category', category)
+    .limit(1)
+
+    if(existingErr) {return res.status(500).json({message : existingErr.message})}
+
+    if(!existing || existing.length === 0 ) {return res.status(404).json("No Category found for Inventory Item")}
+
+    const {data, error} = await supabase
+    .from(table)
+    .select('name, category, cost, unit_measurement')
+    .eq('category', category)
+    .eq('is_deleted', false)
+
+    if(error) {return res.status(500).json({message : existing.message})}
+
+    return res.status(200).json(data)
+})
+
+
+router.get("/get-all", async (req, res) => {
+    const {data, error} = await supabase
+    .from(table)
+    .select('*')
+    .eq('is_deleted', false)
+
+    if(error) {return res.status(500).json({message: error.message})}
+
+    return res.status(200).json(data)
+})
+
+
+router.post("/delete", async (req, res) => {
+    const {skuid} = req.query
+
+    const {data : existing, error : existingErr} = await supabase
+    .from(table)
+    .select('*')
+    .eq('skuid', skuid)
+    .eq('is_deleted', false)
+    .maybeSingle()
+
+    if(existingErr) {return res.status(500).json({message: existingErr.message})}
+    
+    if(!existing) {return res.status(404).json({message : "No inventory item with skuid " + skuid + " found."})}
+
+    const {data , error} = await supabase
+    .from(table)
+    .update({'is_deleted': true})
+    .eq('skuid', skuid)
+    
+
+    if(error) {return res.status(500).json({message : error.message})}
+
+    return res.status(200).send("Deleted Inventory item with SKUID " + skuid)
+    
+})
+
+export default router;
+
+
